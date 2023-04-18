56cb2db3e2be24729df405d275bdaaadfb4fceaa
damianolagana <damianolagana@gmail.com>
2023-04-18 15:53:49 +0200

    array mail

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-let mail = prompt("Inserisci la tua email")
+let mail = prompt("Inserisci la tua email");
 
-console.log("La tua email è: ",mail)
+console.log("La tua email è: ",mail);
+
+const listaMail = ["giusppe.rossi@gmail.com", "fabrizio.biscotti@gmail.com", "alfonso.signorini@gmail.com", "damianolagana@gmail.com"];
