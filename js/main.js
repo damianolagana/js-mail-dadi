36d5f38bcd4af32f39d3c7419c8518f256882b81
damianolagana <damianolagana@gmail.com>
2023-04-18 16:08:15 +0200

    controllo mail con if

--- js/main.js
+++ js/main.js
@@ -5,5 +5,13 @@ console.log("La tua email è: ",mail);
 const listaMail = ["giusppe.rossi@gmail.com", "fabrizio.biscotti@gmail.com", "alfonso.signorini@gmail.com", "damianolagana@gmail.com"];
 
 for (let c = 0; c < listaMail.length; c++){
-    console.log(listaMail[c]);
+    // console.log(listaMail[c]);
+
+    const mailIdonea = listaMail[c];
+    if(mail = mailIdonea){
+        alert("La tua mail : " + mail + "è idonea")
+    } else{
+        alert("La tua email: "+ mail + "non è idonea")
+    }
+
 }
