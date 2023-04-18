b232d172de34d71f576e5469db80ae4c39739613
damianolagana <damianolagana@gmail.com>
2023-04-18 16:00:40 +0200

    ciclo for

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ let mail = prompt("Inserisci la tua email");
 console.log("La tua email è: ",mail);
 
 const listaMail = ["giusppe.rossi@gmail.com", "fabrizio.biscotti@gmail.com", "alfonso.signorini@gmail.com", "damianolagana@gmail.com"];
+
+for (let c = 0; c < listaMail.length; c++){
+    console.log(listaMail[c]);
+}
