92b06e2be997bb760297238c3e21da20479486f0
damianolagana <damianolagana@gmail.com>
2023-04-19 10:32:48 +0200

    email autorizzata check fixed

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 let mail = prompt("Inserisci la tua email");
-
 console.log("La tua email è: ",mail);
 
 const listaMail = ["giusppe.rossi@gmail.com", "fabrizio.biscotti@gmail.com", "alfonso.signorini@gmail.com", "damianolagana@gmail.com"];
+
+let mailAutorizzata = false;
 
 for (let c = 0; c < listaMail.length; c++){
     // console.log(listaMail[c]);
 
     const mailIdonea = listaMail[c];
+   
     if(mail == mailIdonea){
-        alert("La tua mail : " + mail + " è idonea")
-    } else{
-        alert("La tua email: "+ mail + " non è idonea")
+        mailAutorizzata = true;
+        // console.log("Trovata corrispondenza");
+        
     }
 
 }
+
+if(mailAutorizzata==true){
+    alert("La tua email : " + mail + " è autorizzata")
+} else {
+    alert("La tua email : " + mail + " non è autorizzata")
+}
+
